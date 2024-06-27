Fix gradient field

gradient_field.py
@@ -29,21 +29,26 @@ class GradientField(object):
 
         # Now compute the normalized distance vector
         distance_vector = (self.traj[:, index] - np.array([x,y]))
+        distance_norm = np.linalg.norm(distance_vector)
         # Find traj derivative at that index
         derivative = self.traj_derivative(index)
         
-        gradient_x = 1e-5*derivative[0] + 1e-1*distance_vector[0]
-        gradient_y = 1e-5*derivative[1] + 1e-1*distance_vector[1]
+        gradient_x = np.exp(-distance_norm)*derivative[0] + distance_vector[0]
+        gradient_y = np.exp(-distance_norm)*derivative[1] + distance_vector[1]
+        gradient_norm = np.sqrt(gradient_x**2 + gradient_y**2)
+        ortho_vec = np.array([gradient_y, -gradient_x]) / gradient_norm
 
         for collision in self.collisions:
             vec_x = x - collision[0]
             vec_y = y - collision[1]
             vec_norm = np.sqrt(vec_x**2 + vec_y**2)
 
+            direction = np.sign(np.dot([vec_x, vec_y], ortho_vec))
+
             gradient_mag = np.exp(-gamma * vec_norm)
 
-            gradient_x += 5e-1 * vec_x/vec_norm * gradient_mag
-            gradient_y += 5e-1 * vec_y/vec_norm * gradient_mag
+            gradient_x += direction * ortho_vec[0] * gradient_mag
+            gradient_y += direction * ortho_vec[1] * gradient_mag
 
         return gradient_x, gradient_y
     

replan.py
@@ -5,8 +5,11 @@ from gradient_field import GradientField
 
 
 def main():
+    # Number of trajectory points
+    n = 1000
+
     # Init the time vector
-    t = np.linspace(0, 10, 1000)
+    t = np.linspace(0, 10, n)
 
     # Define the polynomial coefficents for the 2d trajectory
     coeff = np.array([[0, 1, 0, 0, 0, 0], # x axis
@@ -25,7 +28,7 @@ def main():
     range_y = (min(gf.traj[1,:]), max(gf.traj[1,:]))
 
     # Plot gradient field
-    resolution = 40
+    resolution = 20
     # Meshgrid 
     x, y = np.meshgrid(np.linspace(range_x[0], range_x[1], resolution),  
                        np.linspace(range_y[0], range_y[1], resolution)) 
@@ -34,19 +37,31 @@ def main():
            np.zeros((resolution, resolution), dtype=float)
     for i in range(x.shape[0]):
         for j in range(x.shape[1]):
-            u1, v1 = gf.field(x[i, j], y[i, j])
+            u1, v1 = gf.field(x[i, j], y[i, j], gamma=0.99)
             u[i, j] = u1
             v[i, j] = v1
     
     # Find new trajector
     traj_new = np.zeros_like(gf.traj)
     traj_new[:, 0] = gf.traj[:, 0]
-    for i in range(1, len(t)):
+    for i in range(1, n):
         dx, dy = gf.traj[0, i]-gf.traj[0,i-1], gf.traj[1, i]-gf.traj[1,i-1] 
         dx_new, dy_new = gf.augment_traj_delta(dx=dx, dy=dy, x=gf.traj[0, i-1], y=gf.traj[1, i-1])
         traj_new[0, i] = gf.traj[0, i] + dx_new
         traj_new[1, i] = gf.traj[1, i] + dy_new
-
+    
+    # Find trajectories from all possible starting points 
+    alt_start_trajs = np.zeros([resolution, resolution, n, 2])
+    for i in range(x.shape[0]):
+        for j in range(y.shape[0]):
+            alt_start_trajs[i, j, 0, 0] = x[i,j]
+            alt_start_trajs[i, j, 0, 1] = y[i,j]
+            for k in range(1, n):
+                dx, dy = gf.field(alt_start_trajs[i,j, k - 1, 0],
+                                  alt_start_trajs[i,j, k - 1, 1],
+                                  gamma=0.5)
+                alt_start_trajs[i, j, k, :] = alt_start_trajs[i, j, k-1, :]  + 10.0/n * np.array([dx, dy])
+    
     fig, ax = plt.subplots(2)
     ax[0].plot(t, gf.traj[0,:], label="x", color="blue")
     ax[0].plot(t, gf.traj[1,:], label="y", color="orange")
@@ -57,8 +72,8 @@ def main():
     ax[0].set_aspect("auto")
     ax[0].legend()
     
-    ax[1].plot(gf.traj[0, :], gf.traj[1, :], label="original", color="blue")
-    ax[1].plot(traj_new[0, :], traj_new[1, :], ":", label="replanned", color="green")
+    ax[1].plot(gf.traj[0, :], gf.traj[1, :], label="original", color="black")
+    #ax[1].plot(traj_new[0, :], traj_new[1, :], ":", label="replanned", color="green")
     for collision in gf.collisions:
         ax[1].scatter(collision[0], collision[1], marker="x", color="red", label="Collision Location")
     ax[1].quiver(x,y,u,v, label="Gradient Field")
@@ -66,6 +81,10 @@ def main():
     ax[1].set_xlabel("x [m]")
     ax[1].set_ylabel("y [m]")
     ax[1].legend()
+
+    for i in range(x.shape[0]):
+        for j in range(y.shape[0]):
+            ax[1].plot(alt_start_trajs[i, j, :, 0], alt_start_trajs[i, j, :, 1])
 
     fig.set_size_inches((8, 20))
     plt.savefig("collision_based_replanning.png", bbox_inches='tight', dpi=500)