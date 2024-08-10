Fix issues with splitting gradient field + add sinusoidal gradient field

gradient_field.py
@@ -2,11 +2,12 @@ import numpy as np
 from scipy.optimize import minimize
 
 class GradientField(object):
-    def __init__(self, t, coeff, collisions=[]) -> None:
+    def __init__(self, t, coeff, speed=1.0, collisions=[]) -> None:
         self.t = t
         self.coeff = coeff
         self.traj = self.f(t=self.t)
         self.collisions = collisions
+        self.speed = speed
 
     def f(self, t):
         return np.array([np.sum([self.coeff[0, i]*t**i for i in range(len(self.coeff[0]))], axis=0),
@@ -19,7 +20,7 @@ class GradientField(object):
     def add_collision(self, collision: tuple) -> None:
         self.collisions.append(collision)
      
-    def field(self, x, y, gamma=0.99):
+    def field(self, x, y, gamma=0.925):
 
         # Find minimum distance trajectory point
         distances2 = (self.traj[0, :] - x)**2 + (self.traj[1, :] - y)**2
@@ -28,14 +29,30 @@ class GradientField(object):
         # Now compute the normalized distance vector
         distance_vector = (self.f(t_min).flatten() - np.array([x,y]))
         distance_norm = np.linalg.norm(distance_vector)
-        # Find traj derivative at that index
-        derivative = self.df(t_min).flatten()
-        
-        gradient_x = np.exp(-8 * distance_norm)*derivative[0] + np.exp(0.1 * distance_norm)*distance_vector[0] / distance_norm
-        gradient_y = np.exp(-8 * distance_norm)*derivative[1] + np.exp(0.1 * distance_norm)*distance_vector[1] / distance_norm
 
+        if distance_norm > 0:
+            # Find traj derivative at that index
+            derivative = self.df(t_min).flatten()
+            
+            gradient_x = (np.exp(-10*distance_norm)*derivative[0] 
+                        + (np.exp(20*distance_norm)-1)*distance_vector[0] / distance_norm)
+            gradient_y = (np.exp(-10*distance_norm)*derivative[1] 
+                        + (np.exp(20*distance_norm)-1)*distance_vector[1] / distance_norm)
+        else:
+            # Find traj derivative at that index
+            derivative = self.df(t_min).flatten()
+            
+            gradient_x = derivative[0]
+            gradient_y = derivative[1]
+
+
+        vel_norm = np.sqrt(derivative[0]**2 + derivative[1]**2)
+        ortho_vec = np.array([derivative[1], -derivative[0]]) / vel_norm
+
+        # Normalize everything to maintain the same speed everywhere
         gradient_norm = np.sqrt(gradient_x**2 + gradient_y**2)
-        ortho_vec = np.array([gradient_y, -gradient_x]) / gradient_norm
+        gradient_x = gradient_x / gradient_norm * self.speed
+        gradient_y = gradient_y / gradient_norm * self.speed
 
         for collision in self.collisions:
             vec_x = x - collision[0]
@@ -44,9 +61,11 @@ class GradientField(object):
 
             direction = np.sign(np.dot([vec_x, vec_y], ortho_vec))
 
-            gradient_mag = 3 * np.exp(-gamma * vec_norm)
+            gradient_mag = (1 - gamma) / vec_norm
 
             gradient_x += direction * ortho_vec[0] * gradient_mag
             gradient_y += direction * ortho_vec[1] * gradient_mag
+
+
 
         return gradient_x, gradient_y

replan.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from gradient_field import GradientField
+from sinusoidal_gradient_field import SinusoidalGradientField
 
 import matplotlib
 
@@ -16,24 +17,27 @@ def main():
     t = np.linspace(0, 1, n)
 
     # Define the polynomial coefficents for the 2d trajectory
-    coeff = np.array([[0, 10, 0, 0, 0, 0], # x axis
-                      [0*10, 18.11667*10,
-                    -13.25*10**2, 3.59375*10**3,
-                    -0.40625*10**4, 0.01614583*10**5]], # y axis
+    coeff = np.array(
+        [[1, 2*np.pi, 0],
+         [1, 2*np.pi, np.pi / 2]],
+        #[[0, 0],
+        # [0, 1]],
+        #[[0, 10, 0, 0, 0, 0], # x axis
+        #[0*10, 18.11667*10, -13.25*10**2, 3.59375*10**3, -0.40625*10**4, 0.01614583*10**5]], # y axis
                      dtype=float) 
     
     # Init the gradient field class
-    gf = GradientField(t=t, coeff=coeff)
+    gf = SinusoidalGradientField(t=t, coeff=coeff)
 
     # Add random collisions along the path
-    collisions_loc = np.zeros([4, 2])
-    for i in range(4):
-        collisions_loc[i, :] =  gf.f(0.2 * (i+1))
+    collisions_loc = np.zeros([1, 2])
+    for i in range(1):
+        collisions_loc[i, :] =  gf.f(0.3 * (i+1))
         gf.add_collision(collisions_loc[i, :])
     
     # Generate the trajectory
-    range_x = (min(gf.traj[0,:])-1, max(gf.traj[0,:])+1)
-    range_y = (min(gf.traj[1,:])-1, max(gf.traj[1,:])+1)
+    range_x = (min(gf.traj[0,:])-0.5, max(gf.traj[0,:])+0.5)
+    range_y = (min(gf.traj[1,:])-0.1, max(gf.traj[1,:])+0.1)
 
     # Plot gradient field
     resolution = 40
@@ -50,16 +54,17 @@ def main():
             v[i, j] = v1
     
     # Find trajectories from all possible starting points 
-    alt_start_trajs = np.zeros([resolution, resolution, n, 2])
+    alt_start_trajs = np.zeros([resolution, resolution, 10*n, 2])
     for i in range(0, x.shape[0], 5):
         for j in range(0, y.shape[0], 5):
-            alt_start_trajs[i, j, 0, 0] = x[i,j]
-            alt_start_trajs[i, j, 0, 1] = y[i,j]
-            for k in range(1, n):
+            x0 = np.random.uniform(low=range_x[0], high=range_x[1])
+            y0 = np.random.uniform(low=range_y[0], high=range_y[1])
+            alt_start_trajs[i, j, 0, 0] = x0
+            alt_start_trajs[i, j, 0, 1] = y0
+            for k in range(1, 10*n):
                 dx, dy = gf.field(alt_start_trajs[i,j, k - 1, 0],
-                                  alt_start_trajs[i,j, k - 1, 1],
-                                  gamma=0.5)
-                alt_start_trajs[i, j, k, :] = alt_start_trajs[i, j, k-1, :]  + 10.0/n * np.array([dx, dy])
+                                  alt_start_trajs[i,j, k - 1, 1])
+                alt_start_trajs[i, j, k, :] = alt_start_trajs[i, j, k-1, :]  + 1.0/n * np.array([dx, dy])
     
     fig, ax = plt.subplots()
     ax.plot(gf.traj[0, :], gf.traj[1, :], label="Original Trajectory", color="black")
@@ -77,9 +82,10 @@ def main():
         for j in range(0, y.shape[0], 5):
             ax.plot(alt_start_trajs[i, j, :, 0],
                     alt_start_trajs[i, j, :, 1],
-                    zorder=0,
+                    zorder=2,
                     linestyle="--",
                     color="#00A6D6")
+
     
     fig.set_size_inches((8, 20))
     plt.savefig("collision_based_replanning.png", bbox_inches='tight', dpi=500)