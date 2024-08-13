80e60b32be7723a5fff3ad4bdf557e5751e24359
Anton Bredenbeck <a.bredenbeck@tudelft.nl>
2024-08-13 12:03:53 -0700

    Change Gradient Field Function
    We now use reciprocal functions instead of exponentials, since
    they are easier to tune / their weights relate better to the
    avoidance radius.
    For the collision contribution we also project the point source
    into the plane, defined by the trajectory velocity as its normal
    avoiding the influence on the progress along the trajectory

--- gradient_field.py
+++ gradient_field.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.optimize import minimize
+from scipy.spatial.transform import Rotation
 
 class GradientField(object):
     def __init__(self, t, coeff, speed=1.0, collisions=[]) -> None:
@@ -42,8 +42,21 @@ class GradientField(object):
 
         return min(max(t, 0), 1)
 
+    def get_orthogonal_unit_vec(self, vec):
+        v = np.zeros(3)
+        if np.isclose(vec, [1, 0, 0]).all():
+            v = np.array([0, 1, 0])
+        elif np.isclose(vec, [0, 1, 0]).all():
+            v = np.array([0, 0, 1])
+        elif np.isclose(vec, [0, 0, 1]).all():
+            v = np.array([1, 0, 0])
 
-    def field(self, x, y, z, gamma=0.925):
+        ortho = np.cross(vec, v)
+
+        return ortho / np.linalg.norm(ortho)
+
+    def field(self, x, y, z,
+              kappa=10.0):
 
         # Find minimum distance trajectory point
         t_min = self.find_nearest_t(np.array([x, y, z]))
@@ -55,18 +68,30 @@ class GradientField(object):
         # Find traj derivative at that index
         derivative = self.df(t_min).flatten()
 
-        if distance_norm > 0:
-            gradient_x = (np.exp(-1*distance_norm)*derivative[0] 
-                        + (np.exp(10*distance_norm)-1)*distance_vector[0] / distance_norm)
-            gradient_y = (np.exp(-1*distance_norm)*derivative[1] 
-                        + (np.exp(10*distance_norm)-1)*distance_vector[1] / distance_norm)
-            gradient_z = (np.exp(-1*distance_norm)*derivative[2] 
-                        + (np.exp(10*distance_norm)-1)*distance_vector[2] / distance_norm)
-        else:            
-            gradient_x = derivative[0]
-            gradient_y = derivative[1]
-            gradient_z = derivative[2]
+        gradient_x = 0
+        gradient_y = 0
+        gradient_z = 0
 
+        if distance_norm > 0:
+            gradient_x += (1.0 / (kappa * distance_norm) * derivative[0]
+                        + kappa * distance_norm * distance_vector[0] / distance_norm)
+            gradient_y += (1.0 / (kappa * distance_norm) * derivative[1]
+                        + kappa * distance_norm * distance_vector[1] / distance_norm)
+            gradient_z += (1.0 / (kappa * distance_norm) * derivative[2]
+                        + kappa * distance_norm * distance_vector[2] / distance_norm)
+        elif t_min >= 1:
+            gradient_x = kappa * distance_norm * distance_vector[0] / distance_norm
+            gradient_y = kappa * distance_norm * distance_vector[1] / distance_norm
+            gradient_z = kappa * distance_norm * distance_vector[2] / distance_norm
+        else:            
+            gradient_x += derivative[0]
+            gradient_y += derivative[1]
+            gradient_z += derivative[2]
+
+        # Find the point source contribution of the collision point
+        coll_contrib_x = 0
+        coll_contrib_y = 0
+        coll_contrib_z = 0
         for collision in self.collisions:
             vec_x = x - collision[0]
             vec_y = y - collision[1]
@@ -74,9 +99,46 @@ class GradientField(object):
 
             vec_norm = np.sqrt(vec_x**2 + vec_y**2 + vec_z**2)
 
-            gradient_x += 0.4 / vec_norm**2 * vec_x / vec_norm  
-            gradient_y += 0.4 / vec_norm**2 * vec_y / vec_norm  
-            gradient_z += 0.4 / vec_norm**2 * vec_z / vec_norm
+            coll_contrib_x += 0.5 * kappa / vec_norm**2 * vec_x / vec_norm
+            coll_contrib_y += 0.5 * kappa / vec_norm**2 * vec_y / vec_norm
+            coll_contrib_z += 0.5 * kappa / vec_norm**2 * vec_z / vec_norm
+
+        # Project the point source contribution into the plane defined
+        # by the trajectory velocity as its normal. That way we do not interfere
+        # with the travelling velocity along the trajectory and only add contributions
+        # laterally to that velocity
+
+        # Find the unit vector of the desired velocity
+        vel_dir = np.array(derivative) / np.linalg.norm(derivative)
+        # Avoid numerical issues if the connection vector and the
+        # velocity vector are exactly parallel
+        coll_contrib = np.array([coll_contrib_x, coll_contrib_y, coll_contrib_z])
+        if np.linalg.norm(coll_contrib) > 0:
+            normal_grad = np.dot([coll_contrib_x,
+                                  coll_contrib_y,
+                                  coll_contrib_z], vel_dir) * vel_dir
+            if np.abs(np.dot(coll_contrib, vel_dir) / np.linalg.norm(coll_contrib) - 1) < 1e-5:
+                normal_dir = normal_grad / np.linalg.norm(normal_grad)
+                ortho_dir_1 = self.get_orthogonal_unit_vec(normal_dir)
+                # Randomly rotate the direction to ensure distribution over all orthogonal
+                # directions that are possible
+                ortho_dir_1 = (
+                    Rotation.from_rotvec(
+                        normal_dir * np.random.uniform(-np.pi, np.pi)
+                    ).as_matrix() @ ortho_dir_1
+                )
+                coll_contrib_x = np.linalg.norm(coll_contrib) * ortho_dir_1[0]
+                coll_contrib_y = np.linalg.norm(coll_contrib) * ortho_dir_1[1]
+                coll_contrib_z = np.linalg.norm(coll_contrib) * ortho_dir_1[2]
+            else:
+                coll_contrib_x = coll_contrib_x - normal_grad[0]
+                coll_contrib_y = coll_contrib_y - normal_grad[1]
+                coll_contrib_z = coll_contrib_z - normal_grad[2]
+
+        # Add the contribution to the gradient
+        gradient_x += coll_contrib_x
+        gradient_y += coll_contrib_y
+        gradient_z += coll_contrib_z
 
         # Normalize everything to maintain the same speed everywhere
         gradient_norm = np.sqrt(gradient_x**2 + gradient_y**2 + gradient_z**2)
--- replan.py
+++ replan.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.spatial.transform import Rotation
 from gradient_field import GradientField
 from sinusoidal_gradient_field import SinusoidalGradientField
 
@@ -11,19 +12,19 @@ matplotlib.rcParams["font.family"] = "serif"
 
 def main():
     # Number of trajectory points
-    n = 60
+    n = 50
 
     # Init the time vector
     t = np.linspace(0, 1, n)
 
     # Define the polynomial coefficents for the 2d trajectory
     coeff = np.array(
-        [[1, 2*np.pi, 0],
-         [1, 2*np.pi, np.pi / 2],
-         [2, 0, 0]],
-        #[[0, 10],
-        # [0, 10],
-        # [2, 0]],
+        [[2.0, 2*np.pi, 0],
+         [2.0, 2*np.pi, np.pi / 2],
+         [1.0, 0, 0]],
+        #[[-2.0, 4.0],
+        # [0, 0.0],
+        # [1,   0]],
         #[[0, 10, 0, 0, 0, 0], # x axis
         # [0*10, 18.11667*10, -13.25*10**2, 3.59375*10**3, -0.40625*10**4, 0.01614583*10**5], # y-axis
         # [2, 0, 0, 0, 0, 0]], # z axis
@@ -33,17 +34,22 @@ def main():
     gf = SinusoidalGradientField(t=t, coeff=coeff)
 
     # Add random collisions along the path
-    collisions_loc = np.zeros([1, 3])
-    for i in range(1):
-        collisions_loc[i, :] =  gf.f(0.3 * (i+1))
+    collisions_loc = np.zeros([2, 3])
+    for i in range(0, 1, 2):
+        loc = gf.f(0.3 * (i + 1))
+        der = gf.df(0.3 * (i + 1))
+        rot = Rotation.from_rotvec(np.array([0, 0, 1]) * np.arctan2(der[1], der[0])).as_matrix()
+        collisions_loc[i, :] =  loc - rot @ np.array([0, 0.22, 0])
+        collisions_loc[i+1, :] =  loc + rot @ np.array([0, 0.22, 0])
         gf.add_collision(collisions_loc[i, :])
+        gf.add_collision(collisions_loc[i+1, :])
     
     # Generate the trajectory
-    minVal = min(gf.traj.flatten())
-    maxVal = max(gf.traj.flatten())
-    range_x = (minVal-0.1, maxVal+0.1)
-    range_y = (minVal-0.1, maxVal+0.1)
-    range_z = (minVal-0.1, maxVal+0.1)
+    maxVal = max(np.abs(gf.traj.flatten()))
+
+    range_x = (-np.abs(maxVal)-0.25, maxVal+0.25)
+    range_y = (-np.abs(maxVal)-0.25, maxVal+0.25)
+    range_z = (-np.abs(maxVal)-0.25, maxVal+0.25)
 
     # Plot gradient field
     resolution = 6
@@ -65,15 +71,17 @@ def main():
                 w[i, j, k] = w1
     
     # Find trajectories from all possible starting points 
-    alt_start_trajs = np.zeros([20, 3, 10*n])
-    for i in range(20):
+    n_sample_trajs = 100
+    alt_start_trajs = np.zeros([n_sample_trajs, 3, 10 * n])
+
+    for i in range(n_sample_trajs):
         x0 = np.random.uniform(low=range_x[0], high=range_x[1])
         y0 = np.random.uniform(low=range_y[0], high=range_y[1])
         z0 = np.random.uniform(low=range_z[0], high=range_z[1])
         alt_start_trajs[i, 0, 0] = x0
         alt_start_trajs[i, 1, 0] = y0
         alt_start_trajs[i, 2, 0] = z0
-        for t in range(1, 10*n):
+        for t in range(1, 10 * n):
             dx, dy, dz = gf.field(alt_start_trajs[i,0, t - 1],
                                   alt_start_trajs[i,1, t - 1],
                                   alt_start_trajs[i,2, t - 1])
@@ -82,11 +90,11 @@ def main():
     fig = plt.figure()
     ax = fig.add_subplot(projection="3d")
     ax.plot(gf.traj[0, :], gf.traj[1, :], gf.traj[2, :],
-            label="Original Trajectory", color="black")
+            label="Original Trajectory", color="black", alpha=0.8)
     ax.quiver(x,y,z,
               u,v,w,
               label="Gradient Field", color="grey",
-              length=0.05 * np.abs(maxVal - minVal), normalize=True)
+              length=0.05 * np.abs(2 * maxVal), normalize=True)
     ax.scatter(collisions_loc[:, 0],
                collisions_loc[:, 1],
                collisions_loc[:, 2], marker="x", color="#D63000", label="Collision Location")
@@ -99,7 +107,7 @@ def main():
 
     ax.legend()
 
-    for i in range(20):
+    for i in range(n_sample_trajs):
         ax.plot(alt_start_trajs[i, 0, :],
                 alt_start_trajs[i, 1, :],
                 alt_start_trajs[i, 2, :],
@@ -108,7 +116,8 @@ def main():
                 color="#00A6D6")
                 
     fig.set_size_inches((8, 20))
-    plt.savefig("collision_based_replanning.png", bbox_inches='tight', dpi=500)
+    plt.show()
+    #plt.savefig("collision_based_replanning.png", bbox_inches='tight', dpi=500)
 
 if __name__ == "__main__":
     main()
